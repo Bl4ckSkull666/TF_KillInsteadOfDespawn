2146c62802e296b7364a80f22a13c625ab3e2b7c
Bl4ckSkull666 <bl4ckskull666@survival-piraten.de>
2019-09-14 18:48:35 +0200

    First Upload?!

---
+++ KillInsteadOfDespawn.cs
@@ -0,0 +1,26 @@
+﻿using ModAPI.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KillInsteadOfDespawn
+{
+    public class KillInsteadOfDespawn
+    {
+        [ExecuteOnGameStart]
+        private static void Load()
+        {
+            ModAPI.Log.Write("Loading Kill instead of DeSpawn");
+            GameObject obj = new GameObject("__KillInsteadOfDespawn__");
+            obj.AddComponent<sceneTrackerEx>();
+            
+        }
+
+        private void Start()
+        {
+            ModAPI.Log.Write("Starting Kill instead of DeSpawn");
+        }
+    }
+}
---
+++ sceneTrackerEx.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using TheForest.Utils;
+using System.Collections.Generic;
+using System;
+
+namespace KillInsteadOfDespawn
+{
+    public class sceneTrackerEx : sceneTracker
+    {
+        public override void WentLight()
+        {
+            ModAPI.Log.Write("Do WentLight");
+            killCannibals();
+            base.WentLight();
+        }
+
+        public override void WentDark()
+        {
+            ModAPI.Log.Write("Do WentDark");
+            base.WentDark();
+        }
+
+        private void killCannibals()
+        {
+            try
+            {
+                List<GameObject> list = new List<GameObject>(Scene.MutantControler.ActiveWorldCannibals);
+                foreach (GameObject item in Scene.MutantControler.activeInstantSpawnedCannibals)
+                {
+                    if (!list.Contains(item))
+                    {
+                        list.Add(item);
+                    }
+                }
+
+                list.RemoveAll((GameObject o) => o == null);
+                //list.RemoveAll((GameObject o) => o != o.activeSelf);
+                if (list.Count > 0)
+                {
+                    foreach (GameObject item in list)
+                    {
+                        ModAPI.Log.Write("Hit " + item.name + " (" + item.layer + ")");
+                        item.SendMessage("hit", 100, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModAPI.Log.Write("Error in Kill Cannibals. " + ex.Message);
+            }
+        }
+    }
+}
