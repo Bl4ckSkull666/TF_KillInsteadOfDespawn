fe3deb2664293ad1a8f210859cb99b939cd7dcec
Bl4ckSkull666 <bl4ckskull666@survival-piraten.de>
2019-09-14 19:05:39 +0200

    split override method and functions

---
+++ Utils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using TheForest.Utils;
+using UnityEngine;
+
+namespace KillInsteadOfDespawn
+{
+    public class Utils
+    {
+        public static void killCannibals()
+        {
+            try
+            {
+                List<GameObject> list = new List<GameObject>(Scene.MutantControler.ActiveWorldCannibals);
+                foreach (GameObject item in Scene.MutantControler.activeInstantSpawnedCannibals)
+                {
+                    if (!list.Contains(item))
+                    {
+                        list.Add(item);
+                    }
+                }
+
+                list.RemoveAll((GameObject o) => o == null);
+                //list.RemoveAll((GameObject o) => o != o.activeSelf);
+                if (list.Count > 0)
+                {
+                    foreach (GameObject item in list)
+                    {
+                        ModAPI.Log.Write("Hit " + item.name + " (" + item.layer + ")");
+                        item.SendMessage("hit", 100, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModAPI.Log.Write("Error in Kill Cannibals. " + ex.Message);
+            }
+        }
+    }
+}
--- sceneTrackerEx.cs
+++ sceneTrackerEx.cs
@@ -10,7 +10,7 @@ namespace KillInsteadOfDespawn
         public override void WentLight()
         {
             ModAPI.Log.Write("Do WentLight");
-            killCannibals();
+            Utils.killCannibals();
             base.WentLight();
         }
 
@@ -18,36 +18,6 @@ namespace KillInsteadOfDespawn
         {
             ModAPI.Log.Write("Do WentDark");
             base.WentDark();
-        }
-
-        private void killCannibals()
-        {
-            try
-            {
-                List<GameObject> list = new List<GameObject>(Scene.MutantControler.ActiveWorldCannibals);
-                foreach (GameObject item in Scene.MutantControler.activeInstantSpawnedCannibals)
-                {
-                    if (!list.Contains(item))
-                    {
-                        list.Add(item);
-                    }
-                }
-
-                list.RemoveAll((GameObject o) => o == null);
-                //list.RemoveAll((GameObject o) => o != o.activeSelf);
-                if (list.Count > 0)
-                {
-                    foreach (GameObject item in list)
-                    {
-                        ModAPI.Log.Write("Hit " + item.name + " (" + item.layer + ")");
-                        item.SendMessage("hit", 100, SendMessageOptions.DontRequireReceiver);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ModAPI.Log.Write("Error in Kill Cannibals. " + ex.Message);
-            }
         }
     }
 }
